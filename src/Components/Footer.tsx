1b4ec0ec0908d2d51af04d8f7741d33517d0585c
jacidre05 <julie.acidre@gmail.com>
2025-11-09 16:24:06 +0800

    text changed

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>🧭 Let your virtual assistant be your compass in the digital wilderness</p>
+      <p>🧭 Let your virtual assistant be your compass in the digital wilderness ediwow</p>
     </footer>
   );
 }
