dcca0495c25f5090f5dbe9335d88e10ac230338a
JAcidre05 <julie.acidre@gmail.com>
2026-02-26 00:17:54 +0800

    Remove 'ediwow' from footer text

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>🧭 Let your virtual assistant be your compass in the digital wilderness ediwow</p>
+      <p>🧭 Let your virtual assistant be your compass in the digital wilderness</p>
     </footer>
   );
 }
